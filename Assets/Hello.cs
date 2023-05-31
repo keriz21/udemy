f45009cfff3d0a7d91d7bc5e5987e6f3042ba684
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2023-05-31 23:27:31 +0700

    menambahkan variabel

---
+++ Assets/Hello.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    int angkaCoba = 500;
+    string namaPercobaan = "Ikan tongkol";
+    float speed = 4.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello");
+        Debug.Log("test 123 dicoba");
+        Debug.Log("nama saya adalah " + namaPercobaan);
+        Debug.LogWarning("ini bakal ada " + angkaCoba + " iterasi");
+        Debug.LogError("Kecepatannya adalah " + speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
