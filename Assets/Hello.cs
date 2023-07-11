552885e3ceafeb1bae818813fcbba123a7ea8d8a
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2023-07-11 20:48:35 +0700

    add move up down left right, with arrow key

--- Assets/Hello.cs
+++ Assets/Hello.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hello : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+
     int angkaCoba = 500;
     string namaPercobaan = "Ikan tongkol";
     float speed = 4.5f;
@@ -11,11 +13,11 @@ public class Hello : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hello");
-        Debug.Log("test 123 dicoba");
-        Debug.Log("nama saya adalah " + namaPercobaan);
-        Debug.LogWarning("ini bakal ada " + angkaCoba + " iterasi");
-        Debug.LogError("Kecepatannya adalah " + speed);
+        //print("hello");
+        //Debug.Log("test 123 dicoba");
+        //Debug.Log("nama saya adalah " + namaPercobaan);
+        //Debug.LogWarning("ini bakal ada " + angkaCoba + " iterasi");
+        //Debug.LogError("Kecepatannya adalah " + speed);
 
         
     }
@@ -23,14 +25,24 @@ public class Hello : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("tombol spasi tertekan");
-        }
-
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
-            Debug.LogError("tombol up tertekan");
+            myRigidBody.velocity = new Vector2 (0f, 10f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) 
+        {
+            myRigidBody.velocity = new Vector2(0f, -10f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            myRigidBody.velocity = new Vector2(10f, 0f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            myRigidBody.velocity = new Vector2(-10f, 0f);
         }
     }
 }
